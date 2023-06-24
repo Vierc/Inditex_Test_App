62e7fdb107c327321eea04c40e60c54f87dc4a9b
Xavier Roig Roca <xavier.roig.roca@gmail.com>
2023-06-24 20:42:55 +0200

    Episode Page

--- src/components/EpisodePlayer.tsx
+++ src/components/EpisodePlayer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../store/store";
+import "../stylesheets/Episode.css";
+import { Episode } from "../types";
 
-function EpisodePlayer() {
-  return (
-    <div>EpisodePlayer</div>
-  )
+const EpisodePlayer = () => {
+
+  const { episodeId } = useParams();
+  const { episodes } = useAppSelector(state => state.episodes)
+  const episode = episodes.filter( episode => episode.id === episodeId )[0] || {} as Episode
+
+  return(
+    <div>
+      <div className="box-shadow episodeBox">
+        <h3>{episode.name}</h3>
+        <p className="episodeDescription" dangerouslySetInnerHTML={{__html: episode.description}}></p>
+        <hr/>
+        <audio controls className="episodePlayer">
+          <source id="audioPlayer" src={episode.url} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </div>
+  );
 }
 
-export default EpisodePlayer
+export default EpisodePlayer;
